ffbe6d4d9992b94072b963ca8b8102ac4815c21e
Lateef Taiwo <taiwolateef55@gmail.com>
2025-02-16 07:21:19 -0600

    modified:   Backup.ps1

--- Backup.ps1
+++ Backup.ps1
@@ -1,4 +1,15 @@
-#Backup the app folder to a zip file with the current date
+# #Backup the app folder to a zip file with the current date
+# $date = Get-Date -format "yyyy-MM-dd"
+# Compress-Archive -Path './app' -CompressionLevel 'Fastest' -DestinationPath "./backup-$date"
+# Write-Host "Created backup at $('./backup-' + $date + '.zip')"
+
+
+#Use parameters for source and Destination paths to make the script more flexible
+
+Param(
+  [string]$Path = '../app',
+  [string]$DestinationPath = '../'
+)
 $date = Get-Date -format "yyyy-MM-dd"
-Compress-Archive -Path './app' -CompressionLevel 'Fastest' -DestinationPath "./backup-$date"
-Write-Host "Created backup at $('./backup-' + $date + '.zip')"
+Compress-Archive -Path $Path -CompressionLevel 'Fastest' -DestinationPath "$($DestinationPath + 'backup-' + $date)"
+Write-Host "Created backup at $($DestinationPath + 'backup-' + $date + '.zip')"
