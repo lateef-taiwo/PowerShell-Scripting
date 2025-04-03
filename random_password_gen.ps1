e0a18c20b9a9e51c5899fe7dcfcee8aa1dd20c1b
Lateef Taiwo <taiwolateef55@gmail.com>
2025-04-03 18:46:09 -0500

    Added random_password_generator.ps1

---
+++ random_password_gen.ps1
@@ -0,0 +1,17 @@
+# function GeneratePassword
+# {
+# $letterNumberArray = @('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8','9','0', '!', '@', '#', '$', '%', '^', '&', '*')
+# for(($counter=0); $counter -lt 20; $counter++)
+# {
+# $randomCharacter = get-random -InputObject $letterNumberArray
+# $randomString = $randomString + $randomCharacter
+# }
+# return $randomString
+# }
+
+# $RandomPassword = GeneratePassword
+
+$length = 12
+$characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&'
+$password = -join (1..$length | ForEach-Object { $characters[(Get-Random -Maximum $characters.Length)] })
+Write-Output $password
